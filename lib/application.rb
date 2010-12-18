5af35264a0718fce555161c59de7d0ff09ff89d6
Yuzai Chen <mvjome@gmail.com>
2010-12-18 22:34:59 +0800

    Almost finished,  margins needs to be adjusted

--- lib/application.rb
+++ lib/application.rb
@@ -1,45 +1,24 @@
 require 'rubygems'
 require 'hotcocoa'
-
-# Replace the following code with your own hotcocoa code
+framework 'WebKit'
 
 class Application
 
   include HotCocoa
   
   def start
-    application :name => "Ihaveu" do |app|
+    application :name => "ihaveu" do |app|
       app.delegate = self
-      window :frame => [100, 100, 500, 500], :title => "Ihaveu" do |win|
-        win << label(:text => "Hello from HotCocoa", :layout => {:start => false})
+      window :frame => [0, 0, 1350, 800], :title => "优众 - About life, About friend", :style => [:titled, :closable, :miniaturizable] do |win|
         win.will_close { exit }
+        win << web_view(:frame => [0, 0, 1340, 790]) do |wv|
+          wv.url = "http://www.ihaveu.com/chief.html"
+          wv.auto_size
+        end
       end
     end
   end
   
-  # file/open
-  def on_open(menu)
-  end
-  
-  # file/new 
-  def on_new(menu)
-  end
-  
-  # help menu item
-  def on_help(menu)
-  end
-  
-  # This is commented out, so the minimize menu item is disabled
-  #def on_minimize(menu)
-  #end
-  
-  # window/zoom
-  def on_zoom(menu)
-  end
-  
-  # window/bring_all_to_front
-  def on_bring_all_to_front(menu)
-  end
 end
 
 Application.new.start
